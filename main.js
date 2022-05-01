695bbd48bb8501f822b96b03312b13c438029108
KIM DAIN <98942415+dain015@users.noreply.github.com>
2022-05-02 00:15:58 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector('.btn1').addEventListener('click', function(){
+    document.querySelector('#section0').style.transform = 'translate(0)'
+})
+document.querySelector('.btn2').addEventListener('click', function(){
+    document.querySelector('#section0').style.transform = 'translate(-100vw)'
+})
+document.querySelector('.btn3').addEventListener('click', function(){
+    document.querySelector('#section0').style.transform = 'translate(-200vw)'
+})
